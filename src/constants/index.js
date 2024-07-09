99c31b552b206cfd6e15ad0728f6e3c596979c6d
Harsh Ranjan Pathak <72537619+harshnnn@users.noreply.github.com>
2024-07-09 13:39:34 +0530

    project-name

--- src/constants/index.js
+++ src/constants/index.js
@@ -187,7 +187,7 @@ const testimonials = [
 
 const projects = [
   {
-    name: "Car Rent",
+    name: "Gym Website",
     description:
       "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
     tags: [
@@ -208,7 +208,7 @@ const projects = [
     source_code_link: "https://github.com/",
   },
   {
-    name: "Job IT",
+    name: "Salon Booking",
     description:
       "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
     tags: [
@@ -229,7 +229,7 @@ const projects = [
     source_code_link: "https://github.com/",
   },
   {
-    name: "Trip Guide",
+    name: "Geo-Sync",
     description:
       "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
     tags: [
